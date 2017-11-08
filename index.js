9468dce5f517b8fccff98c2e19c1b3804a359151
Clara <c_c_456@hotmail.com>
2017-11-08 15:29:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 // CodeUse var... never.
-Use let... when you know the value of a variable will change. For example, a counter variable that starts at 0 and is subsequently incremented to 1, 2, 3, and so on. In the lessons on looping and iteration in JavaScript, let will have its moment in the spotlight.
-Use const... for every other variable.
-Best practice is to always declare variables with const and then, if you later realize that the value has to change over the course of your program, circle back to change it to let. your solution in this!
 
 
 const companyName = 'Scuber';
